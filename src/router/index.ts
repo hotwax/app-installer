70ce337b8d621c2ad61f3bf00f2a715684b0d4a1
Azkya Khan <azkya.khan@hotwax.co>
2022-01-04 15:31:56 +0530

    Removed unwanted pages and their routes (#1xqepm0)

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,26 +1,6 @@
 import { createRouter, createWebHistory } from '@ionic/vue-router';
 import { RouteRecordRaw } from 'vue-router';
-import Home from '@/views/Home.vue'
 import Install from '@/views/Install.vue'
-import Login from '@/views/Login.vue'
-import Settings from "@/views/Settings.vue"
-import store from '@/store'
-
-const authGuard = (to: any, from: any, next: any) => {
-  if (store.getters['user/isAuthenticated']) {
-      next()
-  } else {
-    next("/login")
-  }
-};
-
-const loginGuard = (to: any, from: any, next: any) => {
-  if (!store.getters['user/isAuthenticated']) {
-      next()
-  } else {
-    next("/")
-  }
-};
 
 const routes: Array<RouteRecordRaw> = [
   {
@@ -28,29 +8,10 @@ const routes: Array<RouteRecordRaw> = [
     redirect: '/install'
   },
   {
-    path: '/home',
-    name: 'Home',
-    component: Home,
-    beforeEnter: authGuard
-  },
-  {
     path: '/install',
     name: 'Install',
-    component: Install,
-    beforeEnter: authGuard
+    component: Install
   },
-  {
-    path: '/login',
-    name: 'Login',
-    component: Login,
-    beforeEnter: loginGuard
-  },
-  {
-    path: "/settings",
-    name: "Settings",
-    component: Settings,
-    beforeEnter: authGuard
-  }
 ]
 
 const router = createRouter({
