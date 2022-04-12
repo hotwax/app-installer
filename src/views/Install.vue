180ff5f427c9ad58568caa009b48960cbe0ea7e0
Aditya Sharma <aditya.sharma@hotwax.co>
2022-04-12 13:42:03 +0530

    Implemented logic to navigate to specific app pages based upon the type

--- src/views/Install.vue
+++ src/views/Install.vue
@@ -2,11 +2,11 @@
   <ion-page>
     <ion-content :fullscreen="true">
       <div class="container">
-        <form class="install">
+        <form class="install" @keyup.enter="install(form)" @submit.prevent="install(form)">
           <img src="../assets/images/hc.png" />
 
           <ion-list>
-            <ion-radio-group value="oms">
+            <ion-radio-group v-model="appType">
               <ion-list-header>
                 <ion-label>{{ $t("Apps") }}</ion-label>
               </ion-list-header>
@@ -18,12 +18,12 @@
 
               <ion-item>
                 <ion-label>{{ $t("PDP Pre-order") }}</ion-label>
-                <ion-radio slot="start" value="ppo"></ion-radio>
+                <ion-radio slot="start" value="pdpPreorder"></ion-radio>
               </ion-item>
 
               <ion-item>
                 <ion-label>{{ $t("PDP BOPIS") }}</ion-label>
-                <ion-radio slot="start" value="pbp"></ion-radio>
+                <ion-radio slot="start" value="pdpBopis"></ion-radio>
               </ion-item>
 
                <ion-item lines="none">
@@ -34,12 +34,16 @@
 
             <ion-item lines="full">
               <ion-label position="floating">{{ $t("Shop") }}</ion-label>
-              <ion-input type="text" />
+              <ion-input v-model="shop"
+                name="shopOrigin"
+                type="text"
+                placeholder="shop1.myshopify.com"
+                required />
             </ion-item>
           </ion-list>
 
           <div class="ion-padding">
-            <ion-button fill="outline" expand="block">{{ $t("Install") }}</ion-button>
+            <ion-button type="submit" fill="outline" expand="block">{{ $t("Install") }}</ion-button>
           </div>
         </form>
       </div>
@@ -61,6 +65,8 @@ import {
   IonRadioGroup
 } from '@ionic/vue';
 import { defineComponent } from 'vue';
+import { showToast } from '@/utils'
+import { translate } from '@/i18n'
 
 export default defineComponent({
   name: 'Install',
@@ -75,7 +81,30 @@ export default defineComponent({
     IonPage,
     IonRadio,
     IonRadioGroup
-  }
+  },
+  data() {
+    return {
+      shop: '',
+      appType: 'oms'
+    };
+  },
+  methods: {
+    install: function () {
+      let appURL;
+      switch(this.appType) {
+        case "pdpPreorder":
+          appURL = "https://shopify-preorder.hotwax.io/install?shop=" + this.shop;
+          window.location.assign(appURL);
+          break;
+        case "pdpBopis":
+          appURL = "https://shopify-bopis.hotwax.io/install?shop=" + this.shop;
+          window.location.assign(appURL);
+          break;
+        default:
+          showToast(translate('App configuration missing'));
+      }
+    }
+  },
 });
 </script>
 
